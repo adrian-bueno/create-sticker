0fff952f2f1d653d7dd55f61ce2081f6cf6366f1
adrianbueno <adrian.bueno@decidesoluciones.es>
2024-08-08 18:39:24 +0200

    modified build: copy to ./dist package.json, readme and license

--- build.js
+++ build.js
@@ -26,21 +26,33 @@ esbuild.build({
 
 // ESM bundle
 esbuild
-  .build({
-    entryPoints: ["src/index.ts"],
-    bundle: true,
-    outfile: "dist/bundle.esm.mjs",
-    outExtension: { ".js": ".mjs" },
-    tsconfig: "./tsconfig.json",
-    platform: "browser",
-    format: "esm",
-    target: ["es6"],
-    platform: "neutral",
-    sourcemap: true,
-    minify: true,
-    define: { "process.env.NODE_ENV": '"production"' },
-  })
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+.build({
+  entryPoints: ["src/index.ts"],
+  bundle: true,
+  outfile: "dist/bundle.esm.mjs",
+  outExtension: { ".js": ".mjs" },
+  tsconfig: "./tsconfig.json",
+  platform: "browser",
+  format: "esm",
+  target: ["es6"],
+  platform: "neutral",
+  sourcemap: true,
+  minify: true,
+  define: { "process.env.NODE_ENV": '"production"' },
+})
+.catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
+
+// Copy package.json removing unnecesary properties
+const packageJson = JSON.parse(fs.readFileSync("./package.json", "utf8"));
+
+delete packageJson.scripts;
+delete packageJson.devDependencies;
+
+fs.writeFileSync("./dist/package.json", JSON.stringify(packageJson, null, 2), "utf8");
+
+// Copy other files
+fs.copyFileSync("README.md", "dist/README.md");
+fs.copyFileSync("LICENSE.md", "dist/LICENSE.md");
